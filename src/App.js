ead73a26695b361bb53fc140781a34fa6114b0af
Szymon Budziski <szymonbudzinski1@gmail.com>
2022-10-17 18:23:15 +0200

    created template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='header'>
+        <input placeholder='Enter location'></input>
+      </div>
+      <div className='main'></div>
+      <div className='footer'></div>
+    </div>
+  );
+}
+
+export default App;

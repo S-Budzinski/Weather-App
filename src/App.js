37a915e16d16a8ca3d1ce5ae9d3808e15de587c5
Szymon Budziski <szymonbudzinski1@gmail.com>
2022-10-17 23:38:39 +0200

    basic weather React app with API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react'
 
 function App() {
+  const [geo_data, setGeo_data] = useState({})
+  const [location, setLocation] = useState('')
+  // API for our project
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=91829c1123ad7833ce2918176a3d6de1`
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setGeo_data(response.data)
+        console.log(response.data)
+      })
+      
+      setLocation('')
+
+    }
+  }
+ 
   return (
     <div className='container'>
       <div className='header'>
-        <input placeholder='Enter location'></input>
+        <input 
+          placeholder='Enter location' 
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+        />
       </div>
-      <div className='main'></div>
+      <div className='main'>
+          <p className='location'>{geo_data.name}</p>
+          <p className='temp'>{geo_data.main.temp.toFixed()}°C</p>
+      </div>
       <div className='footer'></div>
     </div>
   );
